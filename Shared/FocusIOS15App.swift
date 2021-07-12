49d07e20f6ba8f7e256f8c0cb3fc4eae394da9f8
Tariq Almazyad <34104180+TariqAlmazyad@users.noreply.github.com>
2021-07-12 12:24:35 +0300

    Initial Commit

---
+++ Shared/FocusIOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  FocusIOS15App.swift
+//  Shared
+//
+//  Created by Tariq Almazyad on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FocusIOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
